6d0bd531e0b5481317f251070b8ad041b6f4c17e
Butadiene <najimi0510@gmail.com>
2023-04-08 23:40:12 +0900

    fix

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from div_sound import div_sound
 from sound2text import sound2text
 from trans_test import trans_test
 
-openai.api_key = "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"
+openai.api_key = ""
 
 prompt_for_sound2text = "この音声はAという分野についての授業を録音したものです。大学院の研究室で行われており、多数の参加者がいます。"
 prompt_for_summarization = "この文章は、Aという分野についての授業をWhisperを用いて文字起こしを行ったものです。講義の内容を要約してください。"
