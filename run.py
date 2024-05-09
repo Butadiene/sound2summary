059717f0e600c0987ac8c627cb51978f912b2a9b
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2024-05-09 15:59:41 +0900

    使用モデルをGPT-3.5からGPT-4-Turboに変更

--- run.py
+++ run.py
@@ -11,9 +11,9 @@ with open("key.json", "r") as f:
 # Set your OpenAI API key
 openai.api_key = params['api_key']
 
-prompt_for_sound2text = "この音声はプラズマ物理学という分野についての授業を録音したものです。大学院の研究室で行われており、多数の参加者がいます。"
-prompt_for_summarization = "この文章は、プラズマ物理学という分野についての授業をWhisperを用いて文字起こしを行ったものです。講義の内容を要約してください。"
+prompt_for_sound2text = "この音声はプラズマ物理学という分野についての研究室でのセミナーを録音したものです。大学院の研究室で行われており、多数の参加者がいます。"
+prompt_for_summarization = "この文章は、プラズマ物理学という分野についての研究室でのセミナーをWhisperを用いて文字起こしを行ったものです。ホームページの掲載用に，セミナー議事録の内容を要約してください。"
 
-div_sound()
-sound2text(prompt_for_sound2text, language="ja")  # "ja": Japanease, "en":English
-trans_test(prompt_for_summarization, max_tokens=300)
+#div_sound()
+#sound2text(prompt_for_sound2text, language="ja")  # "ja": Japanease, "en":English
+trans_test(prompt_for_summarization, max_tokens=2400)
